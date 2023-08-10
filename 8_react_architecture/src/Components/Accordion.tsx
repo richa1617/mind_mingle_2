4e05e8a36ced3e99c43cd5afa985ba2df21c220b
Richa Mahajan <richamahajan1617@gmail.com>
2023-08-10 15:27:21 +0200

    Added delete functionality

--- 8_react_architecture/src/Components/Accordion.tsx
+++ 8_react_architecture/src/Components/Accordion.tsx
@@ -4,6 +4,9 @@ import CustomButton from "./CustomButton";
 
 interface accordrionProps {
   children: ReactNode;
+  id: Number;
+  item: any[];
+  setItem: React.Dispatch<React.SetStateAction<any[]>>;
 }
 
 function Accordion(props: accordrionProps) {
@@ -12,11 +15,20 @@ function Accordion(props: accordrionProps) {
   function clickHandler() {
     setIsOpen(!isOpen);
   }
+  function deleteHandler() {
+    console.log("delete");
+    let updatedItems = props.item.filter((e) => e.id !== props.id);
+    props.setItem(updatedItems);
+  }
+
   return (
     <div className="accordion">
       {isOpen && props.children}
       <CustomButton variant="secondary" anything={clickHandler}>
         {isOpen ? "close" : "open"}
+      </CustomButton>
+      <CustomButton variant="important" anything={deleteHandler}>
+        Delete
       </CustomButton>
     </div>
   );
--- 8_react_architecture/src/Components/AccordionContainer.tsx
+++ 8_react_architecture/src/Components/AccordionContainer.tsx
@@ -1,19 +1,25 @@
 import { useState } from "react";
 import Accordion from "./Accordion";
-import CustomButton from "./CustomButton";
 
 function AccordionContainer() {
+  const [item, setItems] = useState([
+    { id: 1, title: "Section 1", content: "Content 1" },
+    { id: 2, title: "Section 2", content: "Content 2", extra: "blah" },
+  ]);
+
   return (
     <div>
-      <Accordion>
-        <h1>Hello</h1>
-        <p>Content 1</p>
-      </Accordion>
-
-      <Accordion>
-        <h1>Hello</h1>
-        <p>Content 2</p>
-      </Accordion>
+      {item.map((e) => {
+        // Need id of item
+        // Need to pass setItems
+        return (
+          <Accordion id={e.id} key={e.id} setItem={setItems} item={item}>
+            <h5>{e.title}</h5>
+            <p>{e.content}</p>
+            <p>{e.extra}</p>
+          </Accordion>
+        );
+      })}
     </div>
   );
 }
